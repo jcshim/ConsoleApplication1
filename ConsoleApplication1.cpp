2ebc8165cb61fcfbdd873a991d85e59cf68fbe50
jcshim <jcshim@andong.ac.kr>
2019-11-12 22:39:14 +0900

    초기 커밋.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main(){
+	Mat img_color;
+	VideoCapture cap(0);
+	if (!cap.isOpened()) {
+		cerr << "에러 - 카메라를 열 수 없습니다.\n";
+		return -1;
+	}
+	while (1){
+		cap.read(img_color);
+		if (img_color.empty()) {
+			cerr << "빈 영상이 캡쳐되었습니다.\n";
+			break;
+		}
+		imshow("Color", img_color);
+		if (waitKey(25) >= 0)	break;
+	}
+	return 0;
+}
